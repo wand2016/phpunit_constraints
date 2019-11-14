9cba0bae6a95bef5e2e26af513256231bb1ae892
Daiki Horiyama <d19921207@gmail.com>
2019-11-15 00:27:37 +0900

    骨組み構築

---
+++ src/Constraints/Set.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WandTa\Constraints;
+
+use PHPUnit\Framework\Constraint\Constraint;
+
+/**
+ * 集合
+ */
+class Set extends Constraint
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function toString(): string
+    {
+        return '';
+    }
+}
---
+++ tests/Unit/Constraints/SetTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Constraints;
+
+use PHPUnit\Framework\TestCase;
+
+class SetTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function something_is_something()
+    {
+        $this->assertTrue(true);
+    }
+}
