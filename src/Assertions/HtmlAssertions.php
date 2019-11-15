37da0994690fd6d858830b84bf58cc32da8f38ae
Daiki Horiyama <d19921207@gmail.com>
2019-11-16 01:10:25 +0900

    tweak phpdoc

--- src/Assertions/HtmlAssertions.php
+++ src/Assertions/HtmlAssertions.php
@@ -8,8 +8,19 @@ use PHPUnit\Framework\TestCase;
 use WandTa\Constraints\Html\HtmlNodeCount;
 use WandTa\Constraints\Html\HtmlNodeInnerText;
 
+/**
+ * Assertions on HTML
+ */
 trait HtmlAssertions
 {
+    /**
+     * Asserts that the count of the nodes specified by the given selector
+     * is equal to expected value.
+     * @param string $selector
+     * @param int $expectedCount
+     * @param mixed $html
+     * @return void
+     */
     public function assertHtmlNodeCount(
         string $selector,
         int $expectedCount,
@@ -20,6 +31,14 @@ trait HtmlAssertions
         TestCase::assertThat($html, $constraint);
     }
 
+    /**
+     * Asserts that the innerText of the nodes specified by the given selector
+     * is equal to expected value.
+     * @param string $selector
+     * @param string  $expectedInnerText
+     * @param mixed $html
+     * @return void
+     */
     public function assertHtmlNodeInnerText(
         string $selector,
         string $expectedInnerText,
