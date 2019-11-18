f20f2faac0609b1b8780e10b8d89068f54a57faf
Daiki Horiyama <d19921207@gmail.com>
2019-11-18 21:34:13 +0900

    add AssertHtmlNodeLinksTo Assertion

--- src/Assertions/HtmlAssertions.php
+++ src/Assertions/HtmlAssertions.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace WandTa\Assertions;
 
 use PHPUnit\Framework\TestCase;
+use WandTa\Constraints\Html\HtmlNodeAttribute;
 use WandTa\Constraints\Html\HtmlNodeCount;
 use WandTa\Constraints\Html\HtmlNodeInnerText;
 
 /**
  * Assertions on HTML
+ * @mixin \PHPUnit\Framework\TestCase;
  */
 trait HtmlAssertions
 {
     /**
-     * Asserts that the count of the nodes specified by the given selector
+     * Asserts that the count of the nodes specified by given selector
      * is equal to expected value.
      * @param string $selector
      * @param int $expectedCount
@@ -25,14 +27,13 @@ trait HtmlAssertions
         string $selector,
         int $expectedCount,
         $html
-    ): void
-    {
+    ): void {
         $constraint = new HtmlNodeCount($selector, $expectedCount);
         TestCase::assertThat($html, $constraint);
     }
 
     /**
-     * Asserts that the innerText of the nodes specified by the given selector
+     * Asserts that the innerText of the first node specified by given selector
      * is equal to expected value.
      * @param string $selector
      * @param string  $expectedInnerText
@@ -43,9 +44,25 @@ trait HtmlAssertions
         string $selector,
         string $expectedInnerText,
         $html
-    ): void
-    {
+    ): void {
         $constraint = new HtmlNodeInnerText($selector, $expectedInnerText);
+        TestCase::assertThat($html, $constraint);
+    }
+
+    /**
+     * Asserts that the href attribute of the first node specified by given selector
+     * is equal to expected value.
+     * @param string $selector
+     * @param string $expectedHref
+     * @param mixed $html
+     * @return void
+     */
+    public function assertHtmlNodeLinksTo(
+        string $selector,
+        string $expectedHref,
+        $html
+    ): void {
+        $constraint = new HtmlNodeAttribute($selector, 'href', $expectedHref);
         TestCase::assertThat($html, $constraint);
     }
 }
